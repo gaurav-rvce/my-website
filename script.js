c5c0fa27093b129a38ee99880a22acc71b6d85a4
kumar gaurav <kgaurav@longboatgroupllc.com>
2025-02-10 17:35:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("message").addEventListener("mouseover", function () {
+    this.style.color = "#e74c3c";
+});
+
+document.getElementById("message").addEventListener("mouseout", function () {
+    this.style.color = "#555";
+});
